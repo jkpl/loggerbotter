8caf45652e8e51a5d60325c1bda3af2cf6de1d33
Jaakko Pallari <jkpl@lepovirta.org>
2014-06-01 20:39:44 +0300

    Add jQuery based solution for displaying tag data

--- jsapp/scripts/main.js
+++ jsapp/scripts/main.js
@@ -24,11 +24,51 @@ $(function() {
         series: []
     });
 
+    var tagTabs = $('#tag-tabs')
+    var tagArea = $('#tag-area');
+    var tagElements = {};
+
+    function createTagContainer(tagString) {
+        var container = $('<div></div>').addClass('list-group').hide();
+        var tab = $('<li></li>');
+        var link = $('<a href="#"></a>').text(tagString).click(function(e){
+            showTag(tagString);
+        });
+        tab.append(link);
+        tagTabs.append(tab);
+        tagArea.append(container);
+        tagElements[tagString] = {
+            container: container,
+            tab: tab
+        }
+    }
+
+    function showTag(tagString) {
+        $.each(tagElements, function(k, element) {
+            element.container.hide();
+            element.tab.removeClass('active');
+        });
+        var tagElement = tagElements[tagString];
+        tagElement.container.show();
+        tagElement.tab.addClass('active');
+    }
+
+    function addDataToTag(tagString, data) {
+        var heading = $('<h5></h5>')
+            .addClass('list-group-item-heading')
+            .text(data.heading);
+        var body = $('<p></p>')
+            .addClass('list-group-item-text')
+            .text(data.text);
+        var groupItem = $('<div></div>').addClass('list-group-item')
+            .append(heading).append(body);
+        tagElements[tagString].container.prepend(groupItem);
+    }
+
     var meterdata = new Rx.Subject();
     var ws = new WebSocket('ws://' + location.host + '/ws');
     ws.onmessage = function(event) {
         meterdata.onNext(JSON.parse(event.data));
-        console.log(event.data);
     };
 
     var visitors = meterdata.filter(function(data) {
@@ -42,6 +82,10 @@ $(function() {
         }
     });
 
+    var tags = meterdata.filter(function(data) {
+        return data['meter-id'].startsWith('contains-text');
+    });
+
     visitors.subscribe(function(data) {
         var serie = chart.series.find(function(s) { return s.name === data.place; })
         var point = [data.time, data.peopleCount];
@@ -53,5 +97,14 @@ $(function() {
                 data: [point]
             });
         }
+    });
+
+    tags.subscribe(function(data) {
+        var tagText = data['meter-id'].split('/').slice(1).join('');
+        var data = {heading: data.value.sender.nick, text: data.value.text};
+        if (!tagElements[tagText]) {
+            createTagContainer(tagText);
+        }
+        addDataToTag(tagText, data);
     });
 });
